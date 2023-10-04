74a1c0d0c35d8b6e17ef75785d5b497ec55f26af
Vlad Maslianko <masliankovladik@gmail.ocm>
2023-10-05 00:02:24 +0300

    lab_3 - done

---
+++ lab_3/lab_3.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+	double sum = 0;
+	long n;
+	double current;
+	short k1 = -1;
+	long k2 = 1;
+	double dbln;
+	double esp = 0.000001;
+	for (n = 0; ; n++, k1 = -k1, k2 *= 3) {
+		dbln = static_cast<double>(n);
+		current = k1 * (1 + 3 * dbln) / k2;
+		if (fabs(current) >= esp)
+			sum += current;
+		else
+			break;
+		if (n == 9)
+			cout << "Cума 10 членів послідовності = " << sum << endl;
+	}
+	cout << "Повна сума ряду = " << fixed << setprecision(7) << sum << endl;
+
+
+	return 0;
+}
